282c19827a4de5db6430d15c4d9b881b99d26428
Blade Barringer <blade@crookedneighbor.com>
2016-07-23 09:20:50 -0500

    refactor: Simplify method setup

--- examples/bundle.js
+++ examples/bundle.js
@@ -4,6 +4,26 @@
 var Promise = require('bluebird')
 var widget = require('./vendor/soundcloud-widget')
 
+var WIDGET_METHODS = Object.freeze([
+  'play',
+  'pause',
+  'toggle',
+  'seekTo',
+  'setVolume',
+  'next',
+  'prev',
+  'skip'
+])
+var WIDGET_METHODS_WITH_CALLBACKS = Object.freeze([
+  'getVolume',
+  'getDuration',
+  'getPosition',
+  'getSounds',
+  'getCurrentSound',
+  'getCurrentSoundIndex',
+  'isPaused'
+])
+
 function SoundcloudWidget (iframe) {
   this._widget = widget(iframe)
 }
@@ -23,22 +43,14 @@ SoundcloudWidget.prototype.on = applyFunction('bind')
 SoundcloudWidget.prototype.bind = SoundcloudWidget.prototype.on
 SoundcloudWidget.prototype.removeListener = applyFunction('unbind')
 SoundcloudWidget.prototype.unbind = SoundcloudWidget.prototype.removeListener
-SoundcloudWidget.prototype.play = applyFunction('play')
-SoundcloudWidget.prototype.pause = applyFunction('pause')
-SoundcloudWidget.prototype.toggle = applyFunction('toggle')
-SoundcloudWidget.prototype.seekTo = applyFunction('seekTo')
-SoundcloudWidget.prototype.setVolume = applyFunction('setVolume')
-SoundcloudWidget.prototype.next = applyFunction('next')
-SoundcloudWidget.prototype.prev = applyFunction('prev')
-SoundcloudWidget.prototype.skip = applyFunction('skip')
 
-SoundcloudWidget.prototype.getVolume = applyPromisifiedFunction('getVolume')
-SoundcloudWidget.prototype.getDuration = applyPromisifiedFunction('getDuration')
-SoundcloudWidget.prototype.getPosition = applyPromisifiedFunction('getPosition')
-SoundcloudWidget.prototype.getSounds = applyPromisifiedFunction('getSounds')
-SoundcloudWidget.prototype.getCurrentSound = applyPromisifiedFunction('getCurrentSound')
-SoundcloudWidget.prototype.getCurrentSoundIndex = applyPromisifiedFunction('getCurrentSoundIndex')
-SoundcloudWidget.prototype.isPaused = applyPromisifiedFunction('isPaused')
+WIDGET_METHODS.forEach(function (method) {
+  SoundcloudWidget.prototype[method] = applyFunction(method)
+})
+
+WIDGET_METHODS_WITH_CALLBACKS.forEach(function (method) {
+  SoundcloudWidget.prototype[method] = applyPromisifiedFunction(method)
+})
 
 function applyFunction (originalName) {
   return function () {
